d3368807df182df3a42538d883e052ff934ef79f
duvanleal96 <duvanleal65@gmail.com>
2022-05-25 16:35:29 -0500

    se crea metodo suma y resta

--- ejercicioCalculadora/src/main/java/calculadora/Calculator.java
+++ ejercicioCalculadora/src/main/java/calculadora/Calculator.java
@@ -1,4 +1,43 @@
 package calculadora;
 
+import java.util.logging.Logger;
+import java.util.Scanner;
 public class Calculator {
+    Logger logger = Logger.getLogger("logger");
+    Scanner sc = new Scanner(System.in);
+    private double numberone;
+    private double numbertwo;
+     double result;
+
+    public void operation(){
+        int op;
+        logger.info(()->"ingrese el primer valor ");
+        numberone=sc.nextDouble();
+        logger.info(()->"ingrese el segundo valor ");
+        numbertwo=sc.nextDouble();
+        do {
+            logger.info(()->"¿Que operacion desea realizar \n 1. suma \n 2. resta \n 3. multiplicacion \n 4. division \n 5. salir" );
+             op= sc.nextInt();
+             switch (op){
+                 case 1:
+                     sum();
+                     break;
+                 case 2:
+                     rest();
+                     break;
+                 default:
+                     logger.info(()->"operador no valido");
+                     break;
+             }
+        }while (op!=5);
+    }
+
+    public void sum(){
+        result=numberone+numbertwo;
+        logger.info(()->" la suma de la operacion es " + result);
+    }
+    public void rest(){
+        result=numberone-numbertwo;
+        logger.info(()->" la resta de la operacion es " + result);
+    }
 }

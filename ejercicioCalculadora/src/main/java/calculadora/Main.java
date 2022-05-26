01d5f3bc80936baf664bc6e4dd2e8de1a01ba8d7
JEAR <jhedacro@gmail.com>
2022-05-25 20:48:07 -0500

    Se agrega documentacion

--- ejercicioCalculadora/src/main/java/calculadora/Main.java
+++ ejercicioCalculadora/src/main/java/calculadora/Main.java
@@ -1,5 +1,12 @@
 package calculadora;
 
+/**
+ * Clase Main
+ * Esta clase permite ejecutar el programa para realizar los procesos
+ *
+ * @Author: Duvan Leal - John Acevedo
+ * @Version: v1-25/05/22
+ */
 public class Main {
     public static void main(String[] args) {
         Calculator c = new Calculator();
